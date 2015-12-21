3052a38676e0c1066dfa145cc29370b762d6ea5b
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-21 11:23:07 +0000

    Add test specs for interpolations.

--- test/rSpec.js
+++ test/rSpec.js
@@ -13,17 +13,35 @@ r`			remove
 					tabs`,
 			r`
 			remove
-			tabs`
+			tabs`,
+			
+			r`
+			remove
+			remove
+				tabs
+					tabs`,
+					
+			r`remove${'\r\n\t'}tabs${'\r\n\t\t'}tabs`, //window
+			
+			r`
+			${'remove'}
+				${'tabs'}
+					${'tabs'}`
 		];
 		
 		var expected = [
 			'remove\ntabs',
 			'remove\n\ttabs\n\t\ttabs',
-			'remove\ntabs'
+			'remove\ntabs',
+			'remove\nremove\n\ttabs\n\t\ttabs',
+			'remove\n\ttabs\n\t\ttabs',
+			'remove\n\ttabs\n\t\ttabs',
 		];
 		
 		for(var i = 0 ; i < tests.length ; i++) {
 			expect(tests[i]).to.equal(expected[i]);
+			
+			
 		}
 	})
 })
