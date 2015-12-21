d681f7541762e8bc2b7fae210e986d28327ef2e5
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-21 11:01:36 +0000

    Support top-tagged way.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function r(strings) {
 	var str = strings.raw[0];
 	
-	var tabs = str.match(/^[\t]*/)[0];
+	str = str.replace(/\r\n/g, '\n');
 	
-	return str.substr(tabs.length).replace(new RegExp('\n'+tabs, 'g'), '\n');
+	var newlineAndTabs = str.match(/^\n[\t]*/);
+	
+	
+	if( newlineAndTabs != null ) {
+		var matched = newlineAndTabs[0];
+		
+		str = str.replace(new RegExp(matched, 'g'), '\n').substr(1);
+	}
+	
+	else {
+		var matched = str.match(/^[\t]*/)[0];
+		
+		str = str.substr(matched.length).replace(new RegExp('\n'+matched, 'g'), '\n');
+	}
+	
+	return str;
 }
 
 module.exports = r;
