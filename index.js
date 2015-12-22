8a7c1e89a7029b6fcb97edff62987ae2618944fa
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-22 23:53:00 +0000

    Fix a mistake.(repeated variable assign; var str = str = ...) Concentrate unnecessary line to another.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function r() {
 	var strings = arguments[0];
 	var values = Array.prototype.slice.call(arguments, 1);
 	
-	var str = str = strings.raw.reduce(function(prev, cur, idx) {
+	// concentrate strings and interpolations
+	var str = strings.raw.reduce(function(prev, cur, idx) {
 		return prev + values[idx-1] + cur;
-	});
+	})
+	.replace(/\r\n/g, '\n');
 	
-	
-	str = str.replace(/\r\n/g, '\n');
 	
 	var newlineAndTabs = str.match(/^\n[\t]*/);
 	
