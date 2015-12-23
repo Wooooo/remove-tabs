9e9ddbefcc714cb899e97f80dd41d4f6cb10bab3
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-23 02:48:15 +0000

    Support for umd.

--- index.js
+++ index.js
@@ -30,4 +30,13 @@ function r() {
 	return str;
 }
 
-module.exports = r;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory;
+    } else {
+        // Browser globals (root is window)
+        root["removeTabs"] = factory;
+  }
+}(this, r));
