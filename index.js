d2f716a73d1c38b382cc47b2b0e6b4064bc257bf
Taewoo Kim <xodn4195+c9@gmail.com>
2015-12-23 00:15:28 +0000

    Add comments for dev and clear codes.

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 function r() {
-	var strings = arguments[0];
-	var values = Array.prototype.slice.call(arguments, 1);
+	var strings = arguments[0];									// template strings
+	var values = Array.prototype.slice.call(arguments, 1);	// interpolation values 
 	
 	// concentrate strings and interpolations
 	var str = strings.raw.reduce(function(prev, cur, idx) {
 		return prev + values[idx-1] + cur;
 	})
-	.replace(/\r\n/g, '\n');
+	.replace(/\r\n/g, '\n'); // replace for window newline \r\n
 	
 	
+	// check top-tagged applied
 	var newlineAndTabs = str.match(/^\n[\t]*/);
 	
 	
 	if( newlineAndTabs != null ) {
 		var matched = newlineAndTabs[0];
 		
-		str = str.replace(new RegExp(matched, 'g'), '\n').substr(1);
+		str = str.replace(new RegExp(matched, 'g'), '\n')
+				.substr(1);
 	}
 	
 	else {
 		var matched = str.match(/^[\t]*/)[0];
 		
-		str = str.substr(matched.length).replace(new RegExp('\n'+matched, 'g'), '\n');
+		str = str.substr(matched.length)
+				.replace(new RegExp('\n'+matched, 'g'), '\n');
 	}
 	
 	return str;
